66d3c80ba0648a2a4398c014581bc0b39bfe167a
bonniegachiengu <bonniegachiengu@gmail.com>
2025-02-18 05:36:52 +0300

    successful finder import

---
+++ crawler/crawler.py
@@ -0,0 +1,6 @@
+from finder.finder import Finder
+
+finder = Finder()
+
+# Run the Finder process with specified directory and extensions
+finder.run(directory=r'E:\Films\Movies\Action', extensions=('mp4', 'mkv', 'avi'))
